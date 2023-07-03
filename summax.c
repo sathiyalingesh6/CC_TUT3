ae5a137dffaa60e8c375e1f4e28000bb1265fecf
S FAIZ BANU <116164987+Faizsheik@users.noreply.github.com>
2023-07-03 13:54:30 +0530

    Add files via upload

---
+++ summax.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <omp.h>
+int main()
+{
+    int i, a[5] = {1, 2, 3, 4, 8};
+    int sum = 0, max = 0;
+    int nthreads;
+
+    #pragma omp parallel for private(i) shared(sum, max)
+    for (i = 0; i < 5; i++)
+    {
+        sum = sum + a[i];
+        max = max > a[i] ? max : a[i];
+    }
+
+    nthreads = omp_get_num_threads();
+    printf("Number of threads is: %d\n", nthreads);
+    printf("sum is %d\n", sum);
+    printf("Maximum in an array is %d\n", max);
+
+    return 0;
+}
+
